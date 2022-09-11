551d5b64880f831fa1986208056cfbfc61b2584b
adomerlee <adomerlee@gmail.com>
2022-09-12 02:37:26 +0400

    1808: Display packages

--- device.mk
+++ device.mk
@@ -128,5 +128,17 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0 \
     libsuspend
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    libvulkan \
+    libtinyxml
+
 # Inherit vendor
 $(call inherit-product, vendor/vivo/1808/1808-vendor.mk)
